c5b7817a2e563e34933ec714e72449519be35df7
Léa Harabagiu <elessach@gmail.com>
2019-04-26 23:06:57 +0200

    build files

---
+++ build/precache-manifest.b4bde41c582092ae0883443814051550.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "aef994dfb2b47b342b27",
+    "url": "/static/js/main.f9a518f1.chunk.js"
+  },
+  {
+    "revision": "aef994dfb2b47b342b27",
+    "url": "/static/css/main.d491ba86.chunk.css"
+  },
+  {
+    "revision": "3e1c8828b364bdf43f931fad5f7fb03a",
+    "url": "/index.html"
+  }
+];
